c38a043e4134de43c8c2038143c0e2af9022af36
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-06-01 08:29:04 +0300

    gui added, no conflict occurs
    "gui_frames" = keep this folder under project. Add gitignore for pyache @ahmet
    "application.py" = can use object of it or directly run. If you are going to use its object, delete the object creation on file.

---
+++ gui_frames/inputFrame.py
@@ -0,0 +1,42 @@
+from   tkinter  import ttk
+import tkinter  as tk
+from turtle import st
+
+class InputFrame(tk.Frame) :
+
+    def __init__(self, parent, root, *args, **kwargs) :
+        super().__init__(parent, *args, **kwargs)
+
+        self.root = root
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        
+        self.isFirst = True
+
+        self.style = ttk.Style()
+
+        self.inputArea = tk.Text(self, height=10, width=25, wrap="word", font=("Cambiria", 12, "bold"), foreground="black", background="gray86", selectbackground="black", selectforeground="yellow")
+        self.inputArea.grid(row=0, column=0, sticky="nsew")
+        self.inputArea.focus_force()
+
+        areaScrollBarY = ttk.Scrollbar(self, orient="vertical", command=self.inputArea.yview)
+        areaScrollBarY.grid(row=0, column=1, sticky="nsew")
+
+        self.inputArea.config(yscrollcommand=areaScrollBarY.set)
+
+        self.inputArea.insert(tk.END, "Enter the assembly code here")
+        self.inputArea.bind("<Button-1>", self.clearTextContent)
+
+        self.inputArea.bind("<Delete>", self.clearTextContent)
+    
+    def clearTextContent(self, *event) :
+        self.inputArea.delete("1.0", tk.END)
+        self.inputArea.unbind("<Button-1>")
+
+    def saveAndCloseTextContent(self) :
+        self.inputArea.config(state="disabled")
+
+        with open("data.txt", 'w') as f:
+            f.write(self.inputArea.get("1.0", tk.END).rstrip())
+
