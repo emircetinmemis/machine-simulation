84701a552d5451c4d280eb7b6de49f6f8e438de0
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-04-01 04:02:46 +0300

    bug fix

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from utilities import decode_assembly
-from utilities import RAM, Accumulator, ProgramCounter, InstructionMemory, Compiler
-from graphics import Application
-from utilities import starter, closer
-from constants import CONSOLE_TEXT_PATH, INPUT_TXT_PATH
-import argparse
-import contextlib
+from    utilities   import  RAM, Accumulator, ProgramCounter, InstructionMemory, Compiler
+from    constants   import  CONSOLE_TEXT_PATH, INPUT_TXT_PATH
+from    utilities   import  decode_assembly
+from    utilities   import  starter, closer
+from    graphics    import  Application
+import  contextlib
+import  argparse
 
 def console_application():
     instructions = decode_assembly(INPUT_TXT_PATH)
@@ -17,7 +17,7 @@ def console_application():
 
     while True:
         Compiler.compile_instruction(acc, ram, pc, im)
-        is_manual = True and not(CONSOLE_WRITE)
+        is_manual = True
 
         if is_manual:
             stop_simulation = input('\nPress \'q\' to exit or press \'Enter\' to cycle once.\n')
@@ -33,11 +33,22 @@ def graphical_application():
     app = Application()
     app.mainloop()
 
-def get_application(application_type):
-    if application_type == "gui":
-        return graphical_application
-    elif application_type == "console":
-        return console_application
+def execute_application():
+
+    isGUI, isSaved = get_arguments()
+    
+    if isGUI:
+        if isSaved:
+            with contextlib.redirect_stdout(open(CONSOLE_TEXT_PATH, "w")):
+                graphical_application()
+        else:
+            graphical_application()
+    else:
+        if isSaved:
+            with contextlib.redirect_stdout(open(CONSOLE_TEXT_PATH, "w")):
+                console_application()
+        else:
+            console_application()
 
 
 def get_arguments():
@@ -45,24 +56,14 @@ def get_arguments():
     parser = argparse.ArgumentParser(description='This is a Python-based simulation of a computer processor that includes components such as Memory, Registers, ALU, and a Control Circuit, which reads assembly code and allows users to interact with it through input/output components. The project is designed to be flexible, efficient, and educational, and can be run in a console or GUI interface.')
     parser.add_argument('-g', '--gui', type=int, default=True, help='Set this to False if you want to run the simulation in the console. Default is running on gui.')
     parser.add_argument('-c', '--console-save', type=int, default=False, help='Set this to True if you want to save the console output to a text file. Default is False.')
-    parser.add_argument('-i', '--instructions-path', type=str, help='Set this to the path of the assembly code file you want to run. Default is the file in the data folder.')
-    parser.add_argument('-o', '--console-output-path', type=str, help='Set this to the path of the text file you want to save the console output to. Default is the file in the data folder.')
     args = parser.parse_args()
     
-    return bool(args.console_save), bool(args.gui), args.instructions_path or INPUT_TXT_PATH, args.console_output_path or CONSOLE_TEXT_PATH
+    return bool(args.gui), bool(args.console_save)
 
 if __name__ == "__main__":
     
-    CONSOLE_WRITE, GUI, INPUT_TXT_PATH, CONSOLE_TEXT_PATH = get_arguments()
-    print(f"Console write: {CONSOLE_WRITE}, GUI: {GUI}, Input path: {INPUT_TXT_PATH}, Output path: {CONSOLE_TEXT_PATH}")
     starter()
-    try :
-        if CONSOLE_WRITE:
-            with contextlib.redirect_stdout(open(CONSOLE_TEXT_PATH, "w")):
-                get_application("gui" if GUI else "console")()
-        else:
-            get_application("gui" if GUI else "console")() 
-    except Exception as e:
-        print(f"Exception occured while executing the program: {e}")
+    
+    execute_application()
 
     closer()
